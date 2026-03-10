da2e9e7030a8ef239696963201d36af739b84894
MATANGI SRI PREETHI <165762120+SriPreethiMatangi@users.noreply.github.com>
2026-03-10 10:36:31 +0530

    Added Flask backend

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
-from flask import Flask, request, jsonify
+from flask import Flask,request,jsonify
 
-app = Flask(__name__)
+app=Flask(__name__)
 
-@app.route("/chat", methods=["POST"])
+@app.route("/chat",methods=["POST"])
+
 def chat():
 
-    user_message = request.json["message"]
+    user=request.json["message"].lower()
 
-    if "sad" in user_message:
-        reply = "Try listening to relaxing music."
+    if "sad" in user:
+        reply="Try relaxing music."
 
-    elif "happy" in user_message:
-        reply = "Glad to hear that!"
+    elif "stress" in user:
+        reply="Take deep breaths."
+
+    elif "happy" in user:
+        reply="Great to hear that!"
 
     else:
-        reply = "I'm your AI assistant."
+        reply="I'm your AI assistant."
 
-    return jsonify({"reply": reply})
+    return jsonify({"reply":reply})
 
 
-if __name__ == "__main__":
+if __name__=="__main__":
     app.run(debug=True)
